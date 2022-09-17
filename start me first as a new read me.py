2a0c897c18435ecfb16abbe60ed55792a7529094
FliaIgnacio14 <113742701+FliaIgnacio14@users.noreply.github.com>
2022-09-16 23:40:59 -0300

    Add files via upload

---
+++ start me first as a new read me.py
@@ -0,0 +1,8 @@
+from turtle import *
+import time
+print('Hello! I wannted to say this.')
+circle(130)
+print('This version is too great! I loved so much this WhatScriptVersion!')
+print('Bye!')
+while True:
+	print('')
